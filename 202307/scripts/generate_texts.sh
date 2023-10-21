d81884ef542792fa5597833a2c703b2b946b19f6
imyu <nekotrout@gmail.com>
2023-10-21 23:50:37 +0900

    仮想環境起動方法チェンジ

--- 202307/scripts/generate_texts.sh
+++ 202307/scripts/generate_texts.sh
@@ -7,7 +7,7 @@
 
 source /etc/profile.d/modules.sh
 module load python/3.11 cuda/11.7 cudnn/8.6
-source .venv/bin/activate
+source ~/.venv/bin/activate
 
 export HF_HOME=/scratch/$(whoami)/.cache/huggingface/
 export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
